df16fdea38db349b5d7ec52be81cf6ce580b4046
SuperPims <indexes-benzine0p@icloud.com>
2024-01-08 14:56:17 +0100

    add function binary_tree_insert_right

--- binary_trees.h
+++ binary_trees.h
@@ -22,11 +22,12 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
-/*-------------------FUNCTIONS-----------------------*/
-
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+
+/*-------------------FUNCTIONS-----------------------*/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 #endif
