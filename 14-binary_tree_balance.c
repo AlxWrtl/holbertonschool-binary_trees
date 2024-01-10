539ad96db46dedc51060ef5191dce0ca740fae23
SuperPims <indexes-benzine0p@icloud.com>
2024-01-10 10:44:11 +0100

    declar the left and right height at the begining and move the calcul of the subtrees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -15,13 +15,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0, right_height = 0;
+	
 	/* Handle NULL tree case */
 	if (tree == NULL)
 		return (0);
-
+		
 	/* Calculate height of left and right subtrees */
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	/* Return the greater height plus one */
 	return ((left_height > right_height ? left_height : right_height) + 1);
